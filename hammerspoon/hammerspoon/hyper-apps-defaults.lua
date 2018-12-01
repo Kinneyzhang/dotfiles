4e219ffe8b972bb71bee6793e54f031c7435f3ae
Kinney凯 <kinneyzhang666@gmail.com>
2018-12-01 22:56:36 +0800

    create repo dotfiles

---
+++ hammerspoon/hammerspoon/hyper-apps-defaults.lua
@@ -0,0 +1,20 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'a', 'Android Studio' },
+  { 'b', 'BaiduNetdisk'},
+  { 'c', 'QQ' },
+  { 'd', 'Adobe Dreamweaver CC 2017' }, 
+  { 'e', 'Emacs' },
+  { 'f', 'Finder' },
+  { 'i', 'iTunes' },
+  { 'm', 'QQ Music' },
+  { 'g', 'Google Chrome' },
+  { 'p', 'System Preferences' },
+  { 's', 'Safari' },
+  { 't', 'iTerm' },
+  { 'y', 'YouTube' },
+}
